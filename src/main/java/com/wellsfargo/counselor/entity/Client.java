0129eab529f062b2271838a50ad04aae5b7c08d2
SeoEun Lee <anytimelse98@gmail.com>
2026-07-21 22:45:15 +0100

    Client class added

--- src/main/java/com/wellsfargo/counselor/entity/Client.java
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -1,13 +1,16 @@
 package com.wellsfargo.counselor.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@Entity
 public class Client {
     @Id
     @GeneratedValue()
     private long clientId;
+
+    @ManyToOne
+    @JoinColumn(name = "advisorId", nullable = false)
+    private Advisor advisorId;
 
     @Column(nullable = false)
     private String firstName;
@@ -28,55 +31,74 @@ public class Client {
 
     }
 
-    public Client(String firstName, String lastName, String address, String phone, String email) {
+    public Client(Advisor advisorId, String firstName, String lastName, String address, String phone, String email) {
+        this.advisorId = advisorId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
         this.phone = phone;
         this.email = email;
     }
-
-    public Long getAdvisorId() {
+    public Long getClientId() {
         return clientId;
     }
 
+    public Advisor getAdvisor() {
+        return advisorId;
+    }
+
+    public void setAdvisor(Advisor advisorId){
+        this.advisorId = advisorId;
+    }
+
+
     public String getFirstName() {
+
         return firstName;
     }
 
     public void setFirstName(String firstName) {
+
         this.firstName = firstName;
     }
 
     public String getLastName() {
+
         return lastName;
     }
 
     public void setLastName(String lastName) {
+
         this.lastName = lastName;
     }
 
     public String getAddress() {
+
         return address;
     }
 
     public void setAddress(String address) {
+
         this.address = address;
     }
 
     public String getPhone() {
+
         return phone;
     }
 
     public void setPhone(String phone) {
+
         this.phone = phone;
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
 }
